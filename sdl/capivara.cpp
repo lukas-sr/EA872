41c665cd1247fff34598587076efe716b675e27c
lukas <lukas-sr@hotmail.com>
2021-09-01 18:12:53 -0300

    adicione aula 1-set

--- sdl/capivara.cpp
+++ sdl/capivara.cpp
@@ -6,18 +6,45 @@ const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 class Model{
-    public:
-        float 
-    private:
-}
+	private:
+		float m,b,k;
+	
+	public: 
+		void set_parametros(float new_m, float new_b, float new_k);
+};
 
+void Model::set_parametros(float new_m, float new_b, float new_k){
+
+	m = new_m;
+	b = new_b;
+	k = new_k;
+	};
+	
 class Controller{
-    public:
+	private:
+	
+	public:
+		float solve(Model M, float T, float new_m, float new_b, float new_k);
+		};
+		
+float Controller::solve(Model M, float T, float new_m, float new_b, float new_k){
+		int  N;
+		float delta=T/100;
+		float ax = 0, t = 0;
+				
+		float x_new, x=0, vx_new, vx=1;
+		N = int(T/delta);
+	
+        //M.set_parametros(1,1,1);
+		//ax = (-g -k*x -b*v)/m;
 
-    private:
-}
-
-
+        for(t=0;t<T;t=t+delta){
+			vx_new = vx + ax*t;
+			x_new = x + vx_new*t;
+			}
+		return x_new;
+		}	
+		
 int main() {
   // Inicializando o subsistema de video do SDL
   if ( SDL_Init (SDL_INIT_VIDEO) < 0 ) {
@@ -49,7 +76,13 @@ int main() {
     SDL_Quit();
     return 1;
   }
+	
+  Model M;
 
+  float m=1,b=0.5,k=1;
+  float T = 10;	
+  M.set_parametros(m,b,k);
+  Controller C;
   // Carregando texturas
   // personagem
   SDL_Texture *texture = IMG_LoadTexture(renderer, "./capi.png");
@@ -58,8 +91,8 @@ int main() {
 
   // Quadrado onde a textura sera desenhada
   SDL_Rect target;
-  target.x = 0;
-  target.y = 0;
+  target.x = 100;
+  target.y = 100;
   SDL_QueryTexture(texture, nullptr, nullptr, &target.w, &target.h);
 
   // Controlador:
@@ -73,13 +106,17 @@ int main() {
   while(rodando) {
     // Polling de eventos
     SDL_PumpEvents(); // atualiza estado do teclado
-    
+  
+  
+    target.x -= C.solve(M,T,m,b,k);
+
+
     /*
-    if (state[SDL_SCANCODE_LEFT]) target.x -= 3;
-    if (state[SDL_SCANCODE_RIGHT]) target.x += 3;
-    if (state[SDL_SCANCODE_UP]) target.y -= 3;
-    if (state[SDL_SCANCODE_DOWN]) target.y += 3;
-    */
+    if (state[SDL_SCANCODE_LEFT]) target.x -= 1;
+    if (state[SDL_SCANCODE_RIGHT]) target.x += 1;
+    if (state[SDL_SCANCODE_UP]) target.y -= 1;
+    if (state[SDL_SCANCODE_DOWN]) target.y += 1;
+*/
 
     while (SDL_PollEvent(&evento)) {
       if (evento.type == SDL_QUIT) {
